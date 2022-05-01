a38ca3c221fdc9a92cb9902b0f07ec2d239672bf
ori <ori_larusa@hotmail.com>
2022-05-01 17:07:08 -0300

    estoy generalizando

---
+++ app.py
@@ -0,0 +1,35 @@
+# comienzo
+from email import message
+from importlib.resources import path
+from flask import Flask, make_response, render_template, request,send_from_directory
+import os
+from entorno import config
+from modules.renderizado import CustomRouter
+
+# compresion de la app
+from flask_compress import Compress
+# CORS
+from flask_cors import CORS, cross_origin
+
+# creando la instancia del compresor
+compress = Compress()
+
+# instanciando el controlador que se encarga de renderizar
+rutas = CustomRouter()
+
+##Configuracion del app
+app = Flask(__name__)
+
+app.config.from_object(config)
+
+CORS(app)
+
+compress.init_app(app)
+
+#colocar las rutas antes de correr el servidor
+
+@app.route('/')
+def Index():
+    return rutas.render_index()
+###########################
+###########################
---
+++ modules/mock-data.py
@@ -0,0 +1,32 @@
+seccion=[{
+        "donde": "Estás en la sección 1 de 6",
+        "lugar": "República de los niños", 
+        "descripcion": "Found in Akaka Falls State Park, Akaka Falls is a magnificent sight, cascading 135 metres from a cliff in lush, misty rainforest. Twice the height of Niagara Falls, the free-falling Akaka Falls have a strong flow year-round", 
+        },
+        {
+        "donde": "Estás en la sección 2 de 6",
+        "lugar": "República de los niños",
+        "descripcion": "With lava flows dating back some 250,000 years ago, Kilauea is the youngest of the 5 active volcanoes in Hawaii – but it's also the feistiest" 
+        },
+        {
+        "donde": "Estás en la sección 3 de 6",
+        "lugar": "República de los niños", 
+        "descripcion": "Molokai island’s towering sea cliffs give it a dramatic landscape, but there’s just as much beauty to behold in the depths of the sea waters that surround it"
+        },
+        {
+        "donde": "Estás en la sección 4 de 6",
+        "lugar": "República de los niños", 
+        "descripcion": "lol"
+        },
+        {
+        "donde": "Estás en la sección 5 de 6",
+        "lugar": "República de los niños", 
+        "descripcion": "lol"
+        },
+        {
+        "donde": "Estás en la sección 6 de 6",
+        "lugar": "República de los niños", 
+        "descripcion": "lol"
+        },
+
+        ]
