dfaefdabc2aa22b64a561c943424991edb564f58
ori <ori_larusa@hotmail.com>
2022-05-18 21:18:19 -0300

    estuve mejorando el codigo ya esta generico, falta colocar bien los puntos cambiarles los nombres y agreagarles una I de info

--- modules/mock_data.py
+++ modules/mock_data.py
@@ -1,32 +1,40 @@
 seccion=[{
+        "id":"1",
+        "puntos":[["3","Conocé las noticias de la inundación en la TV en ese momento."],["6","Conocé 4 casos testimoniales impactantes."]],
         "donde": "Estás en la sección 1 de 6",
         "lugar": "República de los niños", 
-        "descripcion": "Found in Akaka Falls State Park, Akaka Falls is a magnificent sight, cascading 135 metres from a cliff in lush, misty rainforest. Twice the height of Niagara Falls, the free-falling Akaka Falls have a strong flow year-round", 
+          
         },
         {
+        "id":"2",
+        "puntos":[["5","El peor momento de la inundación. Accedé a las entrevistas realizadas por zonas inundadas."]],
         "donde": "Estás en la sección 2 de 6",
         "lugar": "República de los niños",
-        "descripcion": "With lava flows dating back some 250,000 years ago, Kilauea is the youngest of the 5 active volcanoes in Hawaii – but it's also the feistiest" 
         },
         {
+        "id":"3",
+        "puntos":[["3","Observá el simulador de nivel de agua de inundación."],["1","observá un compilado de fotos impactantes de la inundación."],["4","Jugá con la Sandbox AR y explora el efecto según el tipo de relieve."]],
         "donde": "Estás en la sección 3 de 6",
         "lugar": "República de los niños", 
-        "descripcion": "Molokai island’s towering sea cliffs give it a dramatic landscape, but there’s just as much beauty to behold in the depths of the sea waters that surround it"
+        
         },
         {
+        "id":"4",
+        "puntos":[["2","Descubrí los diarios del momento, mirá las fotos compartidas por las personas damnificadas."],["4_2","Informate sobre los motivos de por qué se inundan estas zonas."],["4_3","Prepararse es fundamental. Aprendé recomendaciones de como actuar, y conocé los centros de evacuación más cercanos."]],
         "donde": "Estás en la sección 4 de 6",
         "lugar": "República de los niños", 
-        "descripcion": "lol"
         },
         {
+        "id":"5",
+        "puntos":[["5_1","Se debe pensar antes de actuar. Prepará de manera correacta tu mochila de emergencia."],["5_2","Accedé a juegos educativos realizados por estudiantes de la UNLP."]],
         "donde": "Estás en la sección 5 de 6",
         "lugar": "República de los niños", 
-        "descripcion": "lol"
         },
         {
+        "id":"6",
+        "puntos":[["6_1","Ingresá al sitio web de Citadine con entrevistas de otros países y con soluciones basadas en la naturaleza. Conocé más sobre las inundaciones urbanas en el exterior."],["6_2""Ingresá a los sitios web de Citadine con entrevistas de otros países y con soluciones basadas en la naturaleza. Conocé más sobre las inundaciones urbanas en el exterior."]],
         "donde": "Estás en la sección 6 de 6",
         "lugar": "República de los niños", 
-        "descripcion": "lol"
         },
 
         ]
