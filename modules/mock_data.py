f514585e8629342cf49abcf08389c5daec5f0437
ori <ori_larusa@hotmail.com>
2022-05-19 08:17:54 -0300

    pude mejorar las imagenes de los puntos falta colocar un icono de informacion a los puntos

--- modules/mock_data.py
+++ modules/mock_data.py
@@ -1,26 +1,26 @@
 seccion=[{
         "id":"1",
-        "puntos":[["3","Conocé las noticias de la inundación en la TV en ese momento."],["6","Conocé 4 casos testimoniales impactantes."]],
+        "puntos":[["1_1","Conocé las noticias de la inundación en la TV en ese momento."],["1_2","Conocé 4 casos testimoniales impactantes."]],
         "donde": "Estás en la sección 1 de 6",
         "lugar": "República de los niños", 
           
         },
         {
         "id":"2",
-        "puntos":[["5","El peor momento de la inundación. Accedé a las entrevistas realizadas por zonas inundadas."]],
+        "puntos":[["2_1","El peor momento de la inundación. Accedé a las entrevistas realizadas por zonas inundadas."]],
         "donde": "Estás en la sección 2 de 6",
         "lugar": "República de los niños",
         },
         {
         "id":"3",
-        "puntos":[["3","Observá el simulador de nivel de agua de inundación."],["1","observá un compilado de fotos impactantes de la inundación."],["4","Jugá con la Sandbox AR y explora el efecto según el tipo de relieve."]],
+        "puntos":[["3_1","Observá el simulador de nivel de agua de inundación."],["3_2","Observá un compilado de fotos impactantes de la inundación."],["3_3","Jugá con la Sandbox AR y explora el efecto según el tipo de relieve."]],
         "donde": "Estás en la sección 3 de 6",
         "lugar": "República de los niños", 
         
         },
         {
         "id":"4",
-        "puntos":[["2","Descubrí los diarios del momento, mirá las fotos compartidas por las personas damnificadas."],["4_2","Informate sobre los motivos de por qué se inundan estas zonas."],["4_3","Prepararse es fundamental. Aprendé recomendaciones de como actuar, y conocé los centros de evacuación más cercanos."]],
+        "puntos":[["4_1","Descubrí los diarios del momento, mirá las fotos compartidas por las personas damnificadas."],["4_2","Informate sobre los motivos de por qué se inundan estas zonas."],["4_3","Prepararse es fundamental. Aprendé recomendaciones de como actuar, y conocé los centros de evacuación más cercanos."]],
         "donde": "Estás en la sección 4 de 6",
         "lugar": "República de los niños", 
         },
@@ -32,7 +32,7 @@ seccion=[{
         },
         {
         "id":"6",
-        "puntos":[["6_1","Ingresá al sitio web de Citadine con entrevistas de otros países y con soluciones basadas en la naturaleza. Conocé más sobre las inundaciones urbanas en el exterior."],["6_2""Ingresá a los sitios web de Citadine con entrevistas de otros países y con soluciones basadas en la naturaleza. Conocé más sobre las inundaciones urbanas en el exterior."]],
+        "puntos":[["6_1","Ingresá al sitio web de Citadine con entrevistas de otros países y con soluciones basadas en la naturaleza. Conocé más sobre las inundaciones urbanas en el exterior."]],
         "donde": "Estás en la sección 6 de 6",
         "lugar": "República de los niños", 
         },
