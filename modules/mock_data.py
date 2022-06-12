e1c245f7261c4f1f995e64b042827cfc9b74608e
ori <ori_larusa@hotmail.com>
2022-06-12 11:40:01 -0300

    se cambio el texto seccion con stand

--- modules/mock_data.py
+++ modules/mock_data.py
@@ -1,39 +1,39 @@
 seccion=[{
         "id":"1",
         "puntos":[["1_1","Conocé 4 casos testimoniales impactantes."],["1_2","Conocé las noticias de la inundación en la TV en ese momento."]],
-        "donde": "Estás en la sección 1 de 6",
+        "donde": "Estás en el stand 1 de 6",
         "lugar": "República de los niños", 
           
         },
         {
         "id":"2",
         "puntos":[["2_1","El peor momento de la inundación. Accedé a las entrevistas realizadas por zonas inundadas."]],
-        "donde": "Estás en la sección 2 de 6",
+        "donde": "Estás en el stand 2 de 6",
         "lugar": "República de los niños",
         },
         {
         "id":"3",
         "puntos":[["3_1","Observá el simulador de nivel de agua de inundación."],["3_2","Observá un compilado de fotos impactantes de la inundación."],["3_3","Jugá con la Sandbox AR y explora el efecto según el tipo de relieve."]],
-        "donde": "Estás en la sección 3 de 6",
+        "donde": "Estás en el stand 3 de 6",
         "lugar": "República de los niños", 
         
         },
         {
         "id":"4",
         "puntos":[["4_1","Descubrí los diarios del momento, mirá las fotos compartidas por las personas damnificadas."],["4_2","Informate sobre los motivos de por qué se inundan estas zonas."],["4_3","Prepararse es fundamental. Aprendé recomendaciones de como actuar, y conocé los centros de evacuación más cercanos."]],
-        "donde": "Estás en la sección 4 de 6",
+        "donde": "Estás en el stand 4 de 6",
         "lugar": "República de los niños", 
         },
         {
         "id":"5",
         "puntos":[["5_1","Se debe pensar antes de actuar. Prepará de manera correacta tu mochila de emergencia."],["5_2","Accedé a juegos educativos realizados por estudiantes de la UNLP."]],
-        "donde": "Estás en la sección 5 de 6",
+        "donde": "Estás en el stand 5 de 6",
         "lugar": "República de los niños", 
         },
         {
         "id":"6",
         "puntos":[["6_1","Ingresá al sitio web de Citadine con entrevistas de otros países y con soluciones basadas en la naturaleza. Conocé más sobre las inundaciones urbanas en el exterior."]],
-        "donde": "Estás en la sección 6 de 6",
+        "donde": "Estás en el stand 6 de 6",
         "lugar": "República de los niños", 
         },
 
