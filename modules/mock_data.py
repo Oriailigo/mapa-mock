2edfa7fa60536b50c02c2b290824483c0aeb4ec1
ori <ori_larusa@hotmail.com>
2022-07-02 19:51:11 -0300

    cambios de la seccion 5

--- modules/mock_data.py
+++ modules/mock_data.py
@@ -1,41 +1,31 @@
 seccion=[{
         "id":"1",
-        "puntos":[["1_1","Conocé 4 casos testimoniales impactantes."],["1_2","Conocé las noticias de la inundación en la TV en ese momento."]],
-
-        "donde": "Estás en el stand 1 de 6",
-
-        "lugar": "República de los niños", 
-          
+        "puntos":[["1_1","Conocé 4 casos testimoniales impactantes.","Poster"],["1_2","Conocé las noticias de la inundación en la TV en ese momento.","Televisión"]],
+        "lugar": "República de los niños",           
         },
         {
         "id":"2",
-        "puntos":[["2_1","El peor momento de la inundación. Accedé a las entrevistas realizadas por zonas inundadas."]],
-        "donde": "Estás en el stand 2 de 6",
+        "puntos":[["2_1","El peor momento de la inundación. Accedé a las entrevistas realizadas por zonas inundadas.","Mapa de entrevistas"]],
         "lugar": "República de los niños",
         },
         {
         "id":"3",
-        "puntos":[["3_1","Observá el simulador de nivel de agua de inundación."],["3_2","Observá un compilado de fotos impactantes de la inundación."],["3_3","Jugá con la Sandbox AR y explora el efecto según el tipo de relieve."]],
-        "donde": "Estás en el stand 3 de 6",
-        "lugar": "República de los niños", 
-        
+        "puntos":[["3_1","Observá el simulador de nivel de agua de inundación.","Simulador de nivel de agua"],["3_2","Observá un compilado de fotos impactantes de la inundación.","Proyector"],["3_3","Jugá con la Sandbox AR y explora el efecto según el tipo de relieve.","Sandbox"]],     
+        "lugar": "República de los niños",       
         },
         {
         "id":"4",
-        "puntos":[["4_1","Descubrí los diarios del momento, mirá las fotos compartidas por las personas damnificadas."],["4_2","Informate sobre los motivos de por qué se inundan estas zonas."],["4_3","Prepararse es fundamental. Aprendé recomendaciones de como actuar, y conocé los centros de evacuación más cercanos."]],
-        "donde": "Estás en el stand 4 de 6",
+        "puntos":[["4_1","Descubrí los diarios del momento, mirá las fotos compartidas por las personas damnificadas.","Diarios"],["4_2","Informate sobre los motivos de por qué se inundan estas zonas.","Computadora"],["4_3","Prepararse es fundamental. Aprendé recomendaciones de como actuar, y conocé los centros de evacuación más cercanos.","Página web"]], 
         "lugar": "República de los niños", 
         },
         {
         "id":"5",
-        "puntos":[["5_1","Se debe pensar antes de actuar. Prepará de manera correacta tu mochila de emergencia."],["5_2","Accedé a juegos educativos realizados por estudiantes de la UNLP."]],
-        "donde": "Estás en el stand 5 de 6",
+        "puntos":[["5_1","Se debe pensar antes de actuar. Prepará de manera correacta tu mochila de emergencia.","Mochila inteligente."],["5_2","Accedé a juegos educativos realizados por estudiantes de la UNLP.","Juegos educativos."]],       
         "lugar": "República de los niños", 
         },
         {
         "id":"6",
-        "puntos":[["6_1","Ingresá al sitio web de Citadine con entrevistas de otros países y con soluciones basadas en la naturaleza. Conocé más sobre las inundaciones urbanas en el exterior."]],
-        "donde": "Estás en el stand 6 de 6",
+        "puntos":[["6_1","Ingresá al sitio web de Citadine con entrevistas de otros países y con soluciones basadas en la naturaleza. Conocé más sobre las inundaciones urbanas en el exterior.","Web de Citadine"]],   
         "lugar": "República de los niños", 
         },
 
